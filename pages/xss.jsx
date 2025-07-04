3902d119888653c2095fd58fe507d5ccc338ce03
Valentin Panov <valentin.panov@me.com>
2025-07-04 13:19:38 +0200

    /xss v4

--- pages/xss.jsx
+++ pages/xss.jsx
@@ -20,6 +20,8 @@ export default function Xss() {
     <>
       <h2 className={s.title}>XSS.</h2>
       <p>{window?.top[1]?.document?.cookie}</p>
+      <p>{window.frames}</p>
+      <p>{window.top}</p>
       <p className={s.description}>
         <GradientLink to={`/`} text={"Go back to safety"} />
       </p>
